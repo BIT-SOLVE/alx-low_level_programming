f09a76dfa8968ff42fa96b957ce7058a15c4d8ea
BIT-SOLVE <amaosolomon3@gmail.com>
2022-04-10 11:02:54 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,23 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
- *main - print lowercase alpha a-z but remove 'q' and 'e'
- *Return: Always 0 (Success)
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
  */
-
-int main(void)                                                                                    
+int main(void)
 {
-	char alpha = 'a';
-	
-	while (alpha <= 'z')
-	{
-		if ((alpha != 'q') && (alpha != 'e'))
-		{
-			putchar(alpha);
-		}
-		alpha++;
-	}
-	putchar('\n');
-	
-	return (0);                               
+	int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if /n > 1)
+printf("%d is positive\n", n);
+else if (n < 0)
+printf("%d is negative\n", n);
+else
+
+printf("%d is zero\n", n);
+
+return (0);
 }
